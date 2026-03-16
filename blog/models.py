ad6e9d4d1c5fb15b4e9e29f93e7d189e5f637aa0
Aleksan-dres <galeks382@gmail.com>
2026-03-16 23:22:25 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,44 +1,85 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count, Prefetch
+
+
+class CustomQuerySet(models.QuerySet):
+    def year(self, year):
+        posts_at_year = self.filter(
+            published_at__year=year).order_by("published_at")
+        return posts_at_year
+
+    def popular(self):
+        return self.annotate(likes_count=Count("likes", distinct=True)).order_by("-likes_count")
+
+    def fetch_with_comments_count(self):
+        posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(
+            comments_count=Count("comments", distinct=True))
+        count_for_id = {
+            post.id: post.comments_count for post in posts_with_comments}
+
+        for post in self:
+            post.comments_count = count_for_id.get(post.id, 0)
+
+        return list(self)
+
+    def with_prefetched_tags(self):
+        return self.prefetch_related(Prefetch("tags", queryset=Tag.objects.annotate(posts_count=Count("posts"))))
+
+    def with_author(self):
+        return self.select_related("author")
+
+    def fully_optimized(self):
+        return self.with_author().with_prefetched_tags()
+
+
+class TagQuerySet(models.QuerySet):
+    def popular(self):
+        return self.annotate(posts_count=Count("posts")).order_by("-posts_count")
 
 
 class Post(models.Model):
-    title = models.CharField('Заголовок', max_length=200)
-    text = models.TextField('Текст')
-    slug = models.SlugField('Название в виде url', max_length=200)
-    image = models.ImageField('Картинка')
-    published_at = models.DateTimeField('Дата и время публикации')
+    title = models.CharField("Заголовок", max_length=200)
+    text = models.TextField("Текст")
+    slug = models.SlugField("Название в виде url", max_length=200)
+    image = models.ImageField("Картинка")
+    published_at = models.DateTimeField("Дата и время публикации")
 
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор',
-        limit_choices_to={'is_staff': True})
+        verbose_name="Автор",
+        limit_choices_to={"is_staff": True})
     likes = models.ManyToManyField(
         User,
-        related_name='liked_posts',
-        verbose_name='Кто лайкнул',
+        related_name="liked_posts",
+        verbose_name="Кто лайкнул",
         blank=True)
     tags = models.ManyToManyField(
-        'Tag',
-        related_name='posts',
-        verbose_name='Теги')
+        "Tag",
+        related_name="posts",
+        verbose_name="Теги")
+
+    objects = CustomQuerySet.as_manager()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+        return reverse("post_detail", args={"slug": self.slug})
 
     class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
+        ordering = ["-published_at"]
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
 
 
 class Tag(models.Model):
-    title = models.CharField('Тег', max_length=20, unique=True)
+    title = models.CharField("Тег", max_length=20, unique=True)
+
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -47,31 +88,32 @@ class Tag(models.Model):
         self.title = self.title.lower()
 
     def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
+        return reverse("tag_filter", args={"tag_title": self.slug})
 
     class Meta:
-        ordering = ['title']
-        verbose_name = 'тег'
-        verbose_name_plural = 'теги'
+        ordering = ["title"]
+        verbose_name = "тег"
+        verbose_name_plural = "теги"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        'Post',
+        "Post",
         on_delete=models.CASCADE,
-        verbose_name='Пост, к которому написан')
+        verbose_name="Пост, к которому написан",
+        related_name="comments")
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор')
+        verbose_name="Автор")
 
-    text = models.TextField('Текст комментария')
-    published_at = models.DateTimeField('Дата и время публикации')
+    text = models.TextField("Текст комментария")
+    published_at = models.DateTimeField("Дата и время публикации")
 
     def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
+        return f"{self.author.username} under {self.post.title}"
 
     class Meta:
-        ordering = ['published_at']
-        verbose_name = 'комментарий'
-        verbose_name_plural = 'комментарии'
+        ordering = ["published_at"]
+        verbose_name = "комментарий"
+        verbose_name_plural = "комментарии"
